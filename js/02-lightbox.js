b6c933ef5c465c4f793d45cdbe9be526542fc760
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-01-20 23:58:20 +0200

    update 02-gallery.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryCardsImage = imagesGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryCardsImage);
+
+function imagesGallery(cardsImage) {
+  return cardsImage.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+  })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
