25632c370040129442a2a34fb909563cbe81c3f8
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-01-20 23:58:11 +0200

    update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryCardsImage = imagesGallery(galleryItems);
+
+gallery.addEventListener('click', handleClick);
+gallery.insertAdjacentHTML('beforeend', galleryCardsImage);
+
+function imagesGallery(cardsImage) {
+  return cardsImage.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+  })
+    .join('');
+}
+
+function handleClick(event) {
+  const isGalleryImage = event.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+  const modal = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`, {
+    onShow: (instance) => {
+      document.body.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+          instance.close();
+        }
+      })
+    }
+  })
+  modal.show();
+
+  event.preventDefault();
+}
